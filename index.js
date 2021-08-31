042ef1166b4161fc94b9a52c9d227dc4e2d9d204
RakhiPareek268 <87005355+RakhiPareek268@users.noreply.github.com>
2021-08-31 16:41:47 +0530

    Message updation

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello \n'
+  const msg = 'Hello node\n'
   res.end(msg);
 });
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello Rakhi\n'
+	const msg2 = 'Hello again form node\n'
 	res.end(msg2);
 });
 
